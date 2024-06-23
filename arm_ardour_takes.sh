bc972c98ce1a9f3af84a07ad4df43757b8182b41
DrungSturm92 <134954274+DrungSturm92@users.noreply.github.com>
2024-06-23 18:31:04 -0300

    rename channels

--- arm_ardour_takes.sh
+++ arm_ardour_takes.sh
@@ -1,22 +1,51 @@
 #!/bin/bash
 
 ANS=y
-echo $ANS
+# echo $ANS
 
+mkdir "copias"
 while [ $ANS = y  ]; do
-	echo $ANS
+	# echo $ANS
 	echo "Escribí el número de la toma que deseas renombrar"
 	read NUM_VIEJO
 
 	echo "Escribí el nuevo nombre que deseas"
 	read NOMBRE_NUEVO
 
-	mkdir $NOMBRE_NUEVO
+	mkdir ./copias/$NOMBRE_NUEVO
 
 	for i in Take$NUM_VIEJO'_'*; do 
-		mv "$i" ./$NOMBRE_NUEVO/"${i/Take"$NUM_VIEJO"/"$NOMBRE_NUEVO"}";
+		cp "$i" ./copias/$NOMBRE_NUEVO/"${i/Take"$NUM_VIEJO"/"$NOMBRE_NUEVO"}";
 	done
 
-	read -p "Deseas continuar? (y/n)" ANS
-	echo $ANS
+	read -p "Deseas continuar? (y/n) -> " ANS
+	# echo $ANS
 done
+
+read -p "Deseas renombrar canales? (y/n) -> " CAN
+
+cd copias;
+# echo "Estas aquí -> $(pwd)"\;
+
+while [ $CAN = y  ]; do
+        echo "Escribí el nombre del canal que deseas renombrar"
+        read CAN_VIEJO
+
+        echo "Escribí el nuevo nombre que deseas"
+        read CAN_NUEVO
+
+        for d in *; do
+                cd "$d";
+                echo "Estás aquí -> $(pwd)"\;
+		counter=$(ls | grep "$CAN_VIEJO" | wc -l); 
+               	if [ $counter -gt 0 ]; then
+                       	for f in *${CAN_VIEJO}*; do
+                               	echo "Renombrando -> $f"\;
+                               	mv "$f" "${f/"$CAN_VIEJO"/"$CAN_NUEVO"}";
+                       	done
+               	fi
+                cd ..;
+        done
+        read -p "Deseas continuar? (y/n) -> " CAN
+done
+
